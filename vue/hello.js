98e3c7711b48832fba34a49811ac909f5d97f430
jma <johnnyma1988@gmail.com>
2016-03-12 16:05:11 +0800

    helloworld

---
+++ vue/hello.js
@@ -0,0 +1,54 @@
+new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue.js!',
+    message2: 'aa',
+    test: 1
+  }
+})
+
+new Vue({
+  el: '#app-list',
+  data: {
+    todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue.js' },
+      { text: 'Build Something Awesome' }
+    ]
+  }
+})
+
+new Vue({
+  el: '#app-user-input',
+  data: {
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
+
+
+new Vue({
+  el: '#app-full',
+  data: {
+    newTodo: '',
+    todos: [
+      { text: 'Add some todos' }
+    ]
+  },
+  methods: {
+    addTodo: function () {
+      var text = this.newTodo.trim()
+      if (text) {
+        this.todos.push({ text: text })
+        this.newTodo = ''
+      }
+    },
+    removeTodo: function (index) {
+      this.todos.splice(index, 1)
+    }
+  }
+})
