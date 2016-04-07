9d925dbd58a90ac9bad98385cc8c2bd1670f0064
jma <johnnyma1988@gmail.com>
2016-04-07 09:22:51 +0800

    Update code_tips.js

--- code_tips.js
+++ code_tips.js
@@ -95,3 +95,20 @@ DraggableMixin.call(UserItemView.prototype, {
 
 itemView.startDrag();
 
+
+
+
+
+
+function curry(fn, scope) {
+  var scope = scope || window;
+  var args = [];
+
+  for (var i = 2, len = arguments.length; i < len; ++i) {
+    args.push(arguments[i]);
+  };
+
+  return function() {
+    fn.apply(scope, args);
+  };
+}
